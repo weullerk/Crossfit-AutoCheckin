74c525c7a7ff4dcbbad8cd035ca7bb8d2d34c860
Costa <weuller.costa@capgemini.com>
2019-02-09 08:17:22 -0200

    first commit

---
+++ app/src/main/java/com/alienonwork/crossfitcheckin/MainActivity.java
@@ -0,0 +1,51 @@
+package com.alienonwork.crossfitcheckin;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setSelectedItemId(navController.getCurrentDestination().getId());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch(menuItem.getItemId()) {
+                            case R.id.settingsDest:
+                                navController.navigate(R.id.settingsDest, null, new NavOptions.Builder().build());
+                                break;
+                            case R.id.scheduleDest:
+                                navController.navigate(R.id.scheduleDest);
+                                break;
+                            case R.id.historyDest:
+                                navController.navigate(R.id.historyDest);
+                                break;
+                        }
+                        return true;
+                    }
+                }
+        );
+
+
+    }
+
+}
